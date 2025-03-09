06a095db71f1ac03de2c6a1f91e8e6606caf4c15
Amer <m.amer_abutaleb@hotmail.com>
2025-03-09 16:26:39 +0200

    refactor Home widget to StatefulWidget and add a Switch in the AppBar

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        actions: [Switch(value: value, onChanged: onChanged)],
       ),
     );
   }
